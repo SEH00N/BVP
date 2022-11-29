f8e84e2d421ffea4e174bc2e16594499bf3d535f
SEH00N <06psh0501@gmail.com>
2022-11-29 18:19:50 +0900

    webhook test

--- Assets/01. Scripts/Core/GameManager.cs
+++ Assets/01. Scripts/Core/GameManager.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager instance = null;
+    public static GameManager Instance {
+        get {
+            if(instance == null)
+                instance = FindObjectOfType<GameManager>();
 
+            return instance;
+        }
+    }
 }
