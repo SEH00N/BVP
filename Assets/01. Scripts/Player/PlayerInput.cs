83cdc8d31889bd2c1016f7be7ac63b94d4bdde2b
SEH00N <06psh0501@gmail.com>
2022-12-06 19:57:38 +0900

    jump jump man

--- Assets/01. Scripts/Player/PlayerInput.cs
+++ Assets/01. Scripts/Player/PlayerInput.cs
@@ -1,7 +1,11 @@
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    [SerializeField] GameObject jumpEffect = null;
+
     private Movement movement = null;
     private PlayerRotator rotator = null;
 
@@ -27,10 +31,21 @@ public class PlayerInput : MonoBehaviour
         rotator.BodyRotateTo(y);
     }
 
+    [SerializeField] bool onJump = false;
     private void JumpInput()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-            movement.DoJump();
+        if(Input.GetKeyDown(KeyCode.Space) && !onJump)
+        {
+            movement.DoJump(() => {
+                onJump = true;
+                jumpEffect.SetActive(true);
+                jumpEffect.transform.DOScale(Vector3.zero, 0.4f).SetEase(Ease.Linear).OnComplete(() => {
+                    jumpEffect.SetActive(false);
+                    jumpEffect.transform.localScale = Vector3.one;
+                    onJump = false;
+                });
+            });
+        }
     }
 
     private void MovementInput()
