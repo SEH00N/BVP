522fb5cc740e4f3059e0b96651869103343fcdbf
SEH00N <06psh0501@gmail.com>
2022-12-20 23:55:19 +0900

    something

--- Assets/01. Scripts/Boss/Core.cs
+++ Assets/01. Scripts/Boss/Core.cs
@@ -6,12 +6,17 @@ public class Core : MonoBehaviour, IDamageable
     public Color currentColor = Color.gray;
     private Material coreMaterial = null;
 
-        [SerializeField] float maxHp = 100f;
+    [SerializeField] float maxHp = 100f;
     [SerializeField] float currentHp = 0f;
 
     public float CurrentHp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
     public float MaxhHp => throw new NotImplementedException();
+
+    private void Awake()
+    {
+        coreMaterial = GetComponent<MeshRenderer>().material;
+    }
 
     public void OnDamage(float damage, Vector3 hitPos = default, Action callback = null)
     {
@@ -30,6 +35,6 @@ public class Core : MonoBehaviour, IDamageable
     public void SetColor(Color targetColor)
     {
         currentColor = targetColor;
-        coreMaterial.SetColor("", currentColor);
+        coreMaterial.SetColor("_Color", currentColor);
     }
 }
